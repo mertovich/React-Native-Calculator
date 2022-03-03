0b7153b37b789d2d5ae6c786690a52887344181d
mertovich <88425310+mertovich@users.noreply.github.com>
2022-03-03 12:21:47 +0300

    Project start

---
+++ mds_calculator/App.js
@@ -0,0 +1,14 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
