de6bd1b3874582db7dfd0f0592fa24109c502830
mertovich <88425310+mertovich@users.noreply.github.com>
2022-03-03 12:48:13 +0300

    add(BackGroundColor)

--- mds_calculator/App.js
+++ mds_calculator/App.js
@@ -1,14 +1,21 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea}>
       <View>
         <Text>App</Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#22252D',
+    height: '100%',
+  },
+});
 
 export default App;
